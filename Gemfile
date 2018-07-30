3172a013f5dcab399c67004d3cbb98918121b82a
Faith Analog <faith@faithanalog.com>
2018-07-29 20:59:24 -0400

    Add initial page

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'sinatra', '~> 2.0'
 gem 'thin', '~> 1.7'
+gem 'redis', '~> 4.0'
--- savebronycon2019.rb
+++ savebronycon2019.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'redis'
+require 'json'
+
+redis = Redis.new(url: ENV["REDISTOGO_URL"])
+
+PONIES_SAVING_BRONYCON = "ponies_saving_bronycon"
+
+set :public_folder, File.dirname(__FILE__) + '/static'
+set :static, true
+#set :static_cache_control, ???
 
 get '/' do
-  'Derp'
+  redirect '/index.html'
+end
+
+get '/api/how_many_saving_bronycon' do
+  n = redis.get(PONIES_SAVING_BRONYCON).to_i
+  JSON.generate({
+    "ponies_saving_bronycon" => n
+  })
+end
+
+post '/api/save_bronycon' do
+  redis.incr PONIES_SAVING_BRONYCON
+  "You Helped Save BronyCon!"
 end
